8b85cf0d9093c543531cbd83ae5b628154d4b380
Turiano romero <romero.guzman18@gmail.com>
2021-08-18 19:52:18 -0500

    btn del

--- src/screens/Calculator/index.tsx
+++ src/screens/Calculator/index.tsx
@@ -14,26 +14,26 @@ const index = () => {
   };
 
   const buildNumer = (numberText: string) => {
-
     //no aceptar doble punto:
     if (number.includes('.') && numberText === '.') return;
 
     //validar si el numero empieza con 0 o -0
     if (number.startsWith('0') || number.startsWith('-0')) {
-
       //validar si es el primer punto decimal
       if (numberText === '.') {
         setNumber(number + numberText);
-      } else if (numberText === '0' && number.includes('.')) { // evaluar si es otro 0 y hay un punto'.'
+      } else if (numberText === '0' && number.includes('.')) {
+        // evaluar si es otro 0 y hay un punto'.'
         setNumber(number + numberText);
-      } else if (numberText !== '0' && !number.includes('.')) { // evaluar si es diferente de 0 y no tiene un punto '.'
-        setNumber(numberText)
-      } else if (numberText === '0' && !number.includes('.')) { // Evitar el 0000.0
-        setNumber(number)
+      } else if (numberText !== '0' && !number.includes('.')) {
+        // evaluar si es diferente de 0 y no tiene un punto '.'
+        setNumber(numberText);
+      } else if (numberText === '0' && !number.includes('.')) {
+        // Evitar el 0000.0
+        setNumber(number);
       } else {
         setNumber(number + numberText);
       }
-
     } else {
       setNumber(number + numberText);
     }
@@ -41,9 +41,23 @@ const index = () => {
 
   const positiveAndNegatve = () => {
     if (number.includes('-')) {
-      setNumber(number.replace('-', ''))
+      setNumber(number.replace('-', ''));
     } else {
-      setNumber('-' + number )
+      setNumber('-' + number);
+    }
+  };
+
+  const btnDelete = () => {
+    let numTemp = number;
+
+    if (number.includes('-')) {
+      numTemp = number.substr(1); // elimina la primera posicion del string
+    }
+
+    if (numTemp.length > 1) {
+      setNumber(number.slice(0, -1)); // elimina la ultima posicion del string
+    } else {
+      setNumber('0');
     }
   };
 
@@ -69,7 +83,7 @@ const index = () => {
           />
           <Button
             text="del"
-            onPress={clear}
+            onPress={btnDelete}
             color={secondaryColors.gray}
             textColor={primaryColors.black}
           />
